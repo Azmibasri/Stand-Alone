203282bcfbe59ed92c2e4b6692a2a7e97aa323e1
azmibasri <azmibasri391@gmail.com>
2024-10-16 20:12:25 +0700

    Membuat jendela dan icon sementara

---
+++ main.py
@@ -0,0 +1,15 @@
+#Pengaturan window
+import pygame
+pygame.init()
+info = pygame.display.Info()
+width, height = info.current_w, info.current_h
+pygame.display.set_caption("Stand Alone 0.0.1")
+icon = pygame.image.load("Image/UI/Icon.jpeg")
+pygame.display.set_icon(icon)
+pygame.display.set_mode((width,height-35))
+#Game Running
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
