9ecd3fb7fc5990b08d37cac8924f67d432d0fa16
azmibasri <azmibasri391@gmail.com>
2024-10-16 20:49:01 +0700

    Menganti title window

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pygame
 pygame.init()
 info = pygame.display.Info()
 width, height = info.current_w, info.current_h
-pygame.display.set_caption("Stand Alone 0.0.1")
+pygame.display.set_caption("Stand Alone 0.0.2")
 icon = pygame.image.load("Image/UI/Icon.jpeg")
 pygame.display.set_icon(icon)
 pygame.display.set_mode((width,height-35))
