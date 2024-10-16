cef442e5bd06d5e3db6849c0a6a9e16a9a69fcab
azmibasri <azmibasri391@gmail.com>
2024-10-16 21:23:40 +0700

    Menambah lantai

--- main.py
+++ main.py
@@ -6,10 +6,19 @@ width, height = info.current_w, info.current_h
 pygame.display.set_caption("Stand Alone 0.0.2")
 icon = pygame.image.load("Image/UI/Icon.jpeg")
 pygame.display.set_icon(icon)
-pygame.display.set_mode((width,height-35))
+screen = pygame.display.set_mode((500,500))
+#Warna
+white = (255,255,255)
+red = (255,0,0)
 #Game Running
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    
+    center_w = width / 2
+    center_h = height / 2
+    pygame.draw.line(screen,white,(0,480),(500,480),200)
+
+    pygame.display.flip()
